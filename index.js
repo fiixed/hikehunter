f530d27b5cdd739788484aabacde29bb6a4c84fa
Andrew Bell <fiixed@gmail.com>
2023-03-03 16:34:05 -0600

    CRUD - edit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 const Hike = require('./models/hike');
 
 mongoose.connect('mongodb://localhost:27017/hikehunter',
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render("home");
@@ -34,16 +36,30 @@ app.get('/hikes/new', (req, res) => {
     res.render('hikes/new');
 });
 
-app.get('/hikes/:id', async (req, res,) => {
-    const hike = await Hike.findById(req.params.id)
-    res.render('hikes/details', { hike });
-});
-
 app.post('/hikes', async (req, res) => {
     const hike = new Hike(req.body.hike);
     await hike.save();
     res.redirect(`/hikes/${hike._id}`);
 });
+
+app.get('/hikes/:id', async (req, res,) => {
+    const hike = await Hike.findById(req.params.id)
+    res.render('hikes/details', { hike });
+});
+
+app.get('/hikes/:id/edit', async (req, res) => {
+    const hike = await Hike.findById(req.params.id)
+    res.render('hikes/edit', { hike });
+})
+
+app.put('/hikes/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body.hike);
+    const hike = await Hike.findByIdAndUpdate(id, { ...req.body.hike });
+    res.redirect(`/hikes/${hike._id}`)
+});
+
+
 
 
 
