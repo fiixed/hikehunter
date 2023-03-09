d42a515664e8945001f50cbec24749743b48bb60
Andrew Bell <fiixed@gmail.com>
2023-03-09 14:27:48 -0600

    review routes

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { hikeSchema, reviewSchema } = require('./schemas');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Hike = require('./models/hike');
-const Review = require('./models/review');
+
+
 
 const hikes = require('./routes/hikes');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/hikehunter',
     {
@@ -34,39 +33,15 @@ app.use(methodOverride('_method'));
 
 
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
-app.use('/hikes', hikes)
+
+app.use('/hikes', hikes);
+app.use('/hikes/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render("home");
 });
 
-
-
-app.post('/hikes/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const hike = await Hike.findById(req.params.id);
-    const review = new Review(req.body.review);
-    hike.reviews.push(review);
-    await review.save();
-    await hike.save();
-    res.redirect(`/hikes/${hike._id}`);
-}))
-
-app.delete('/hikes/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Hike.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/hikes/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
