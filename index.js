7e0c30019eae5cc2dd187849808fa138d7252b29
Andrew Bell <fiixed@gmail.com>
2023-03-10 14:57:48 -0600

    passport setup and user model

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+
+//const userRoutes = require('./routes/users');
 const hikes = require('./routes/hikes');
 const reviews = require('./routes/reviews');
 
@@ -44,6 +49,13 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
