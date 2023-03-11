87a854e4a42ae8633ee43d5213a0c43dfae92e31
Andrew Bell <fiixed@gmail.com>
2023-03-11 08:58:52 -0600

    link author to hike

--- routes/hikes.js
+++ routes/hikes.js
@@ -24,20 +24,22 @@ router.get('/new', isLoggedIn, (req, res) => {
     res.render('hikes/new');
 });
 
-router.post('/', validateHike, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateHike, catchAsync(async (req, res, next) => {
     const hike = new Hike(req.body.hike);
+    hike.author = req.user._id;
     await hike.save();
     req.flash('success', 'Successfully made a new hike!');
     res.redirect(`/hikes/${hike._id}`);
 }));
 
 router.get('/:id', catchAsync(async (req, res,) => {
-    const hike = await Hike.findById(req.params.id).populate('reviews');
+    const hike = await Hike.findById(req.params.id).populate('reviews').populate('author');
+    console.log(hike);
     if (!hike) {
         req.flash('error', 'Cannot find that hike!');
         return res.redirect('/hikes');
     }
-    res.render('hikes/details', { hike }, );
+    res.render('hikes/details', { hike },);
 }));
 
 router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
