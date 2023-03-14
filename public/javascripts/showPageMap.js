7faf08ac6ac9b07affeaa50c683d33f0b1c5e10e
Andrew Bell <fiixed@gmail.com>
2023-03-14 16:57:14 -0500

    forward geocoding and basic map

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,21 @@
+
+mapboxgl.accessToken = mapToken;
+
+console.log(`what is: ${mapToken}`);
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: hike.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
+});
+
+new mapboxgl.Marker()
+    .setLngLat(hike.geometry.coordinates)
+    // .setPopup(
+    //     new mapboxgl.Popup({ offset: 25 })
+    //         .setHTML(
+    //             `<h3>${hike.title}</h3><p>${hike.location}</p>`
+    //         )
+    // )
+    .addTo(map)
