24a94645c98a2c63d2db2338ef9e8b02d2d5a502
Andrew Bell <fiixed@gmail.com>
2023-03-17 15:06:49 -0500

    fix port

--- index.js
+++ index.js
@@ -159,7 +159,8 @@ app.use((err, req, res, next) => {
 })
 
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log(`Server started on 3000`);
-});
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+})
